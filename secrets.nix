4d3bba14b9456bf280d11abe6edfa918f5bb8df7
Parker Jones <parker.jones@rithum.com>
2025-06-02 22:52:18 -0400

    ok I guess

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,8 @@
 let
-  user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAao6hYRda8Dc88DgWHblVFV/HFCcj6kJuDWq7oqt7Aq";
-  users = [ dustin ];
+  pjones = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAao6hYRda8Dc88DgWHblVFV/HFCcj6kJuDWq7oqt7Aq";
+  users = [ pjones ];
   systems = [ ];
 in
 {
-  #"darwin-syncthing-cert.age".publicKeys = [ dustin ];
-  #"darwin-syncthing-key.age".publicKeys = [ dustin ];
-  #"felix-syncthing-cert.age".publicKeys = [ dustin ];
-  #"felix-syncthing-key.age".publicKeys = [ dustin ];
-  #"github-ssh-key.age".publicKeys = [ dustin ];
-  #"github-signing-key.age".publicKeys = [ dustin ];
-  #"syncthing-gui-password.age".publicKeys = [ dustin ];
+
 }
